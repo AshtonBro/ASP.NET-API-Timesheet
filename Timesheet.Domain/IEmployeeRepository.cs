fe4e4e6b124cf9b908af38cc5231efe0c00ea338
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-03-12 17:10:19 +0500

    Add Employee Service, Interface, Tests isTrue and isFalse

--- Timesheet.Domain/IEmployeeRepository.cs
+++ Timesheet.Domain/IEmployeeRepository.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Timesheet.Domain.Models;
+﻿using Timesheet.Domain.Models;
 
 namespace Timesheet.Domain
 {
     public interface IEmployeeRepository
     {
         StaffEmployee GetEmployee(string lastName);
+        void AddEmployee(StaffEmployee staffEmployee);
     }
 }
---
+++ Timesheet.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Moq;
+using NUnit.Framework;
+using Timesheet.Application.Services;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Tests
+{
+    class EmployeeServiceTests
+    {
+        [Test]
+        [TestCase("Иванов", 20000)]
+        [TestCase("Петров", 30000)]
+        [TestCase("Сидоров", 40000)]
+        public void Add_ShouldReturnTrue(string lastName, int salary)
+        {
+            // Arrange
+            var staffEmployee = new StaffEmployee()
+            {
+                LastName = lastName,
+                Salary = salary
+            };
+
+            var employeeRepository = new Mock<IEmployeeRepository>();
+            employeeRepository.Setup(x => x.AddEmployee(staffEmployee))
+                .Verifiable();
+
+            var service = new EmployeeService(employeeRepository.Object);
+
+            //Act
+            var result = service.AddEmployee(staffEmployee);
+
+            //Assert
+            employeeRepository.Verify(x => x.AddEmployee(staffEmployee), Times.Once);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase("Иванов", 0)]
+        [TestCase("Петров", -1000)]
+        [TestCase("", 40000)]
+        [TestCase(null, 40000)]
+        public void Add_ShouldReturnFalse(string lastName, int salary)
+        {
+            // Arrange
+            var staffEmployee = new StaffEmployee()
+            {
+                LastName = lastName,
+                Salary = salary
+            };
+
+            var employeeRepository = new Mock<IEmployeeRepository>();
+            employeeRepository.Setup(x => x.AddEmployee(staffEmployee))
+                .Verifiable();
+
+            var service = new EmployeeService(employeeRepository.Object);
+
+            //Act
+            var result = service.AddEmployee(staffEmployee);
+
+            //Assert
+            employeeRepository.Verify(x => x.AddEmployee(It.IsAny<StaffEmployee>()), Times.Never);
+            Assert.IsFalse(result);
+        }
+    }
+}
