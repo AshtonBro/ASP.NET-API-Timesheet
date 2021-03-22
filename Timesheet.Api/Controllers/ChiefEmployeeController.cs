7ed62771ceabeaead5c4e3e36b181b0701f2d3f5
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-03-22 17:45:06 +0500

    ChiefEmployee controller

---
+++ Timesheet.Api/Controllers/ChiefEmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChiefEmployeeController : ControllerBase
+    {
+        public readonly IEmployeeService _employeeService;
+
+        public ChiefEmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> AddEmployee(ChiefEmployee chiefEmployee)
+        {
+            return Ok(_employeeService.AddEmployee(chiefEmployee));
+        }
+    }
+}
