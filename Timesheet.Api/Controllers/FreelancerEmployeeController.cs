1a2e4f95286dfb9c5c4a2c810e96c3284b4f4848
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-03-22 17:46:29 +0500

    New Freelanser Employee

---
+++ Timesheet.Api/Controllers/FreelancerEmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FreelancerEmployeeController : ControllerBase
+    {
+        public readonly IEmployeeService _employeeService;
+
+        public FreelancerEmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> AddEmployee(FreelancerEmployee freelancerEmployee)
+        {
+            return Ok(_employeeService.AddEmployee(freelancerEmployee));
+        }
+    }
+}
