a71e3b91132237f1920ce790589836b9d69f3b6e
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-03-22 17:47:25 +0500

    Personal controller

---
+++ Timesheet.Api/Controllers/StaffEmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffEmployeeController : ControllerBase
+    {
+        public readonly IEmployeeService _employeeService;
+
+        public StaffEmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> AddEmployee(StaffEmployee staffEmployee)
+        {
+            return Ok(_employeeService.AddEmployee(staffEmployee));
+        }
+    }
+}
